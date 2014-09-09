5aa72ad02ef657ff048ab21d27470f1e709ed6d6
begumgenc <begumgenc@localhost>
2014-09-09 15:50:57 +0300

    Parameter tuning function has been added

--- src/main/java/org/ivis/io/xml/TestApplet.java
+++ src/main/java/org/ivis/io/xml/TestApplet.java
@@ -95,17 +95,14 @@ public class TestApplet extends Applet implements MouseListener
 		add(layoutButton);
 		add(exitButton);
 
-		//		fileList.add("org/ivis/io/xml/brca1gadd45.xml");
-		//		fileList.add("org/ivis/io/xml/ube2sube3a.xml");
-		//		fileList.add("org/ivis/io/xml/small.xml");
-		//		fileList.add("org/ivis/io/xml/layout.xml");
-		fileList.add("org/ivis/io/xml/insuline.xml");
-		//		fileList.add("org/ivis/io/xml/neuronal.xml");
-		//		fileList.add("org/ivis/io/xml/huaiyu.xml");
-
-		//		fileList.add("org/ivis/io/xml/glycolysis.xml");
-		//		fileList.add("org/ivis/io/xml/adenine.xml");
-		//		fileList.add("org/ivis/io/xml/androgen.xml");
+//		fileList.add("org/ivis/io/xml/small.xml");
+		fileList.add("org/ivis/io/xml/layout.xml");
+//		fileList.add("org/ivis/io/xml/insuline.xml");
+//		fileList.add("org/ivis/io/xml/neuronal.xml");
+//		fileList.add("org/ivis/io/xml/huaiyu.xml");
+//		fileList.add("org/ivis/io/xml/glycolysis.xml");
+//		fileList.add("org/ivis/io/xml/adenine.xml");
+//		fileList.add("org/ivis/io/xml/androgen.xml");
 
 	}
 
